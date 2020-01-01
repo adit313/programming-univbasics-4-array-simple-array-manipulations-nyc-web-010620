9ab668724a6f46161381a54a4cb79981e609315f
Adit P Patel <app2116@gmail.com>
2020-01-01 03:31:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,5 +15,16 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
   array.shift(2)
 end
+
+def using_concat(array1, array2)
+  return array1.concat(array2)
+end
+
+def using_insert(array, element)
+  return array.insert(element, 4)
